5d90cfa0202fe2ec1e2fa251a29c83643ee81047
Hitesh Songra <hgsongra@ymail.com>
2020-04-20 02:22:34 +0530

    Updates

---
+++ precache-manifest.6d6b2451619c5f9261ef73ef54677794.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a9cc9458be16f20547f4342a465e6d",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "0f65395f5571c6f6c7f2",
+    "url": "/hacker-news/static/css/2.478ebdbd.chunk.css"
+  },
+  {
+    "revision": "6ba40ac08e4a98b05409",
+    "url": "/hacker-news/static/css/main.673736af.chunk.css"
+  },
+  {
+    "revision": "0f65395f5571c6f6c7f2",
+    "url": "/hacker-news/static/js/2.4156311f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news/static/js/2.4156311f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba40ac08e4a98b05409",
+    "url": "/hacker-news/static/js/main.76344d1b.chunk.js"
+  },
+  {
+    "revision": "ce66f0af9438000789ec",
+    "url": "/hacker-news/static/js/runtime-main.bf5ac87c.js"
+  },
+  {
+    "revision": "0c44dacf1b038014a6f941131c5e8aa2",
+    "url": "/hacker-news/static/media/progress.0c44dacf.gif"
+  }
+]);
